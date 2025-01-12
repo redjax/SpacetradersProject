062a8d092dcf0833975be638d204de4e2731c9f2
redjax <no@none.com>
2025-01-12 15:50:45 -0500

    Add http_lib.

---
+++ sandbox/test.py
@@ -0,0 +1,13 @@
+import http_lib
+
+if __name__ == "__main__":
+    req = http_lib.build_request(url="https://www.google.com")
+    
+    try:
+        with http_lib.get_http_controller() as http_ctl:
+            res = http_ctl.client.send(req)
+    except Exception as exc:
+        msg = f"({type(exc)}) Error sending request. Details: {exc}"
+        raise exc
+    
+    print(f"Response: [{res.status_code}: {res.reason_phrase}]")
