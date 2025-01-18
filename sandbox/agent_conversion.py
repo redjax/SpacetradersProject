2aa544048d8c59a5538c26dfffdb2422966d1012
redjax <no@none.com>
2025-01-17 22:09:05 -0500

    Fix bugs with agent object conversions.
    Update sandbox example for registering & saving a random agent

--- sandbox/agent_conversion.py
+++ sandbox/agent_conversion.py
@@ -10,7 +10,9 @@ from domain.spacetraders import agent as agent_domain
 
 if __name__ == "__main__":
     setup.setup_loguru_logging(log_level=settings.LOGGING_SETTINGS.get("LOG_LEVEL", default="INFO"))
-    demo_db_engine = db_lib.demo_db.return_demo_engine()
+    db_config = db_lib.demo_db.DEMO_DB_CONFIG
+    log.debug(f"DB config: {db_config}")
+    demo_db_engine = db_lib.demo_db.return_demo_engine(db_conf=db_config)
     setup.setup_database(engine=demo_db_engine)
     
     random_agent_dict = agent_ctl.register_random_agent(retry_on_taken_username=True)
@@ -37,23 +39,17 @@ if __name__ == "__main__":
         log.error(msg)
         
         raise exc
-
-
-    # log.info(f"Converting agent dict to database model")
-    # try:
-    #     random_agent_model = agent_ctl.convert_agent_schema_to_model(agent=random_agent)
-    # except Exception as exc:
-    #     msg = f"({type(exc)}) Error converting registered agent response to database model."
-    #     log.error(msg)
-        
-    #     raise exc
     
-    # log.debug(f"RegisteredAgentModel: {random_agent_model.__dict__}")
-    
-    # log.info("Saving registered agent model to database.")
-    # try:
-    #     agent_ctl.db_client.save_registered_agent_to_db(agent=)
-    # except Exception as exc:
-    #     log.error(f"({type(exc)}) Error saving registered agent to database. Details: {exc}")
+    log.info("Saving registered agent model to database.")
+    try:
+        db_agent = agent_ctl.db_client.save_registered_agent_to_db(agent=random_agent_schema, engine=demo_db_engine)
+        log.success(f"Saved agent {db_agent.symbol} successfully.")
+        log.debug(f"Agent database entity: {db_agent.__dict__}")
+    except Exception as exc:
+        log.error(f"({type(exc)}) Error saving registered agent to database. Details: {exc}")
         
-    #     raise exc
+        raise exc
+    
+    log.info("Converting registered agent from database to RegisteredAgentOut")
+    db_agent_schema: agent_domain.RegisteredAgentOut = agent_ctl.convert_agent_model_to_schema(db_agent=db_agent)
+    log.info(f"Agent from database: {db_agent_schema}")
